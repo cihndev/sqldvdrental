305b9952d686050e7a1080141ed020d428c87173
Cihan Basalan <cihanbasalan@gmail.com>
2022-08-09 13:28:04 +0300

    odev4 dosyası olusturuldu

---
+++ odev4.sql
@@ -0,0 +1,39 @@
+/*
+Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1. film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+2. film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+3. film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+4. country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+5. city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+
+Kolay Gelsin.
+
+*/
+
+-- 1. Sorgu
+
+SELECT DISTINCT replacement_cost FROM film;
+
+-- 2. Sorgu
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+-- 3. Sorgu
+
+SELECT COUNT(DISTINCT title) FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+-- 4. Sorgu
+
+SELECT COUNT(DISTINCT country) FROM country
+WHERE country LIKE '_____';
+
+-- 5. Sorgu
+
+SELECT COUNT(DISTINCT city) FROM city
+WHERE city ILIKE '%r';
+
+-- www.patika.dev
