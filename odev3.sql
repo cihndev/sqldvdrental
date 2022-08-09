b547b8994854d501ede6be45e6a3187b459abf4c
Cihan Basalan <cihanbasalan@gmail.com>
2022-08-09 12:55:28 +0300

    odev3 dosyası olusturuldu

---
+++ odev3.sql
@@ -0,0 +1,34 @@
+/*
+Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1- country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+2- country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+3- film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+4- film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+
+Kolay Gelsin.
+
+*/
+
+-- 1. Sorgu
+
+SELECT * FROM country
+WHERE country ~~ 'A%a';
+
+
+-- 2. Sorgu
+
+SELECT * FROM country
+WHERE country ~~ '_____%n';
+
+-- 3. Sorgu
+
+SELECT title FROM film
+WHERE title ~~* '%t%t%t%t%';
+
+-- 4. Sorgu
+
+SELECT * FROM film
+WHERE title ~~ 'C%' AND length > 90 AND rental_rate = 2.99;
