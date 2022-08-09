18fcc834a736808f9108335ba715bc9be6d4474d
Cihan Basalan <cihanbasalan@gmail.com>
2022-08-09 15:28:24 +0300

    odev6 dosyası olusturuldu

---
+++ odev6.sql
@@ -0,0 +1,35 @@
+/*
+Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1 - film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+2 - film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+3 - film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+4 - film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+
+
+Kolay Gelsin.
+
+*/
+
+-- 1. Sorgu
+
+SELECT ROUND(AVG(rental_rate),3) FROM film;
+
+-- 2. Sorgu
+
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'C%';
+
+-- 3. Sorgu
+
+SELECT MAX(length) FROM film
+WHERE rental_rate = 0.99;
+
+-- 4. Sorgu
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 150;
+
+-- www.patika.dev
